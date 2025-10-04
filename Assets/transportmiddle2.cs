2c7d6e3c99fc0d388278ec0e7b92494d60185863
mhkqwwe1223 <mhkqwwe1223@naver.com>
2025-10-04 15:42:50 +0900

    집 내부 이동

---
+++ Assets/transportmiddle2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transportmiddle2 : MonoBehaviour
+{
+    public Transform spawnPoint4; // 스폰 포인트 설정
+    [SerializeField] float newMinX;
+    [SerializeField] float newMaxX;
+    bool isPlayerNear = false;
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.transform.position = spawnPoint4.position;
+
+            CameraFollowX cam = Camera.main.GetComponent<CameraFollowX>();
+            cam.minX = newMinX;
+            cam.maxX = newMaxX;
+        }
+    }
+}
